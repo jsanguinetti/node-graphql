1a1de2f0c590221b9f5622ba5035b82d7cc44a4b
jsanguinetti <jsangui@gmail.com>
2017-07-20 22:10:41 -0300

    mocking out resolvers, first version of API working with graphiql and all :smile:

---
+++ src/schema/index.js
@@ -0,0 +1,17 @@
+const { makeExecutableSchema } = require('graphql-tools');
+const resolvers = require('./resolvers');
+
+const typeDefs = `
+  type Link {
+    id: ID!
+    url: String!
+    description: String!
+  }
+
+  type Query {
+    allLinks: [Link!]!
+  }
+`;
+
+// Generate the schema object from your types definition.
+module.exports = makeExecutableSchema({typeDefs, resolvers});
