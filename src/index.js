33e3febbf03b15eb8484f1a2002d2066212c24e3
jsanguinetti <jsangui@gmail.com>
2017-07-22 00:03:59 -0300

    using opticsagent analytics

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ const schema = require('./schema');
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 const connectMongo = require('./mongo-connector');
 
+const OpticsAgent = require('optics-agent');
+OpticsAgent.instrumentSchema(schema);
+
 const start = async () => {
   const mongo = await connectMongo();
   let app = express();
+  app.use(OpticsAgent.middleware());
   app.set('port', (process.env.PORT || 5000));
 
-  app.use('/graphql', bodyParser.json(), graphqlExpress({
-    context: { mongo },
-    schema
+  app.use('/graphql', bodyParser.json(), graphqlExpress((req) => {
+    return {
+      context: {
+        mongo,
+        opticsContext: OpticsAgent.context(req),
+      },
+      schema
+    }
   }));
   app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
