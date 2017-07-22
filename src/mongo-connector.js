81f046482e4a2f998be0212895598d9797d77333
jsanguinetti <jsangui@gmail.com>
2017-07-21 23:11:57 -0300

    updated env var MONGO_URL >> MONGODB_URI

--- src/mongo-connector.js
+++ src/mongo-connector.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
 // 1
-const MONGO_URL = process.env.MONGO_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // 2
 module.exports = async () => {
-  const db = await MongoClient.connect(MONGO_URL);
+  const db = await MongoClient.connect(MONGODB_URI);
   return { Links: db.collection('links') };
 }
