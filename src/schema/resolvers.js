b9101807e07676738ad5691ee907fa3fe8fb7e1a
jsanguinetti <jsangui@gmail.com>
2017-07-21 23:09:36 -0300

    removed hardcoded links

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,16 +1,3 @@
-const links = [
-  {
-    id: 1,
-    url: 'http://graphql.org/',
-    description: 'The Best Query Language'
-  },
-  {
-    id: 2,
-    url: 'http://dev.apollodata.com',
-    description: 'Awesome GraphQL Client'
-  },
-];
-
 module.exports = {
   Query: {
     allLinks: async (root, data, { mongo: {Links} }) => {
