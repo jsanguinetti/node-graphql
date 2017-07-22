e53428f96fc4f0facfeb80678167fc9a449426d1
jsanguinetti <jsangui@gmail.com>
2017-07-21 23:29:19 -0300

    using only data object

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -7,7 +7,7 @@ module.exports = {
   Mutation: {
     createLink: async (root, data, {mongo: {Links}}) => {
       const response = await Links.insert(data);
-      return Object.assign({id: response.insertedIds[0]}, data);
+      return data;
     },
   },
   Link: {
