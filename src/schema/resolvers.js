d57673e249503a8c387298c2b36ad6219c258719
jsanguinetti <jsangui@gmail.com>
2017-07-24 20:46:17 -0300

    added votes to links

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -45,6 +45,9 @@ module.exports = {
     postedBy: async ({postedById}, data, {mongo: {Users}}) => {
       return await Users.findOne({_id: postedById});
     },
+    votes: async ({_id}, data, {mongo: {Votes}}) => {
+      return await Votes.find({linkId: _id}).toArray();
+    },
   },
   User: {
     id: root => root._id || root.id,
